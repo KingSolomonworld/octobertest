750b220838019a148970d15b4829d3d1a107e5ff
solomon <sokhifoh@yahoo.com>
2023-10-30 23:24:22 -0600

    prod

--- module-call/main.tf
+++ module-call/main.tf
@@ -1,9 +1,9 @@
 
 module "s3" {
- source = "git::https://github.com/KingSolomonworld/s3-backend-repo.git//s3-module?ref=v1.1.2"
+  source = "git::https://github.com/KingSolomonworld/s3-backend-repo.git//s3-module?ref=v1.1.2"
 }
 
 #module "s3" {
- # source = "git::git@github.com:KingSolomonworld/s3-backend-repo.git//s3-module"
+# source = "git::git@github.com:KingSolomonworld/s3-backend-repo.git//s3-module"
 #}
 
